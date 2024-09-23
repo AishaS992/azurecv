e6684dee3ceec67d05c6dc0d55c165a2c09f4975
“imnotsure1101” <asenkubuge@outlook.com>
2024-09-23 13:31:56 +0100

    Updates to function

--- backend/api/GetCVCounter.cs
+++ backend/api/GetCVCounter.cs
@@ -18,6 +18,7 @@ namespace Company.Function
             [CosmosDB(databaseName: "AzureCV", containerName: "Counter", Connection = "AzureCVConnectionString", Id = "1", PartitionKey = "1")] out Counter updatedCounter,
             ILogger log)
         {
+            // Here is where the counter gets updated.
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             updatedCounter = counter;
